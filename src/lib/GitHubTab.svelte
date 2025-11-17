ec13b59b7b7776c84c5f7427f52d288200d8ce12
LeSofanthiel <neilmccuistion@gmail.com>
2025-11-17 14:20:48 -0700

    Update GitHubTab.svelte

--- src/lib/GitHubTab.svelte
+++ src/lib/GitHubTab.svelte
@@ -10,4 +10,3 @@
 </PanelButton>
 <p>Like WebVM? <a class="underline" href="https://github.com/LeSofanthiel/alpinex11" target="_blank">Give us a star!</a></p>
 <p>WebVM is FOSS, you can fork it to build your own version and begin working on your CheerpX-based project</p>
-<p>Found a bug? Please open a <a class="underline" href="https://github.com/leaningtech/webvm/issues" target="_blank">GitHub issue</a></p>
