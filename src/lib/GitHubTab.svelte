33123283f4adce8cae5a5d8e1bb36236b342f9e8
LeSofanthiel <neilmccuistion@gmail.com>
2025-12-14 13:07:49 -0700

    Update GitHubTab.svelte

--- src/lib/GitHubTab.svelte
+++ src/lib/GitHubTab.svelte
@@ -6,7 +6,7 @@
 <h1 class="text-lg font-bold">GitHub</h1>
 <PanelButton buttonImage="assets/github-mark-white.svg" clickUrl="https://github.com/LeSofanthiel/alpinex11" buttonText="GitHub repo">
 	<i class='fas fa-star fa-xs ml-auto'></i>
-	<span class="ml-1"><GitHubStarCount repo="leaningtech/webvm"/></span>
+	<span class="ml-1"><GitHubStarCount repo="LeSofanthiel/alpinex11"/></span>
 </PanelButton>
 <p>Like WebVM? <a class="underline" href="https://github.com/LeSofanthiel/alpinex11" target="_blank">Give us a star!</a></p>
 <p>WebVM is FOSS, you can fork it to build your own version and begin working on your CheerpX-based project</p
